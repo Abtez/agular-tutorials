a273664d86294b4ae52e28d4c7c9536c1322547f
Abzed <ibraabzed@gmail.com>
2020-11-05 16:14:14 +0300

    add navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { AddTutorialComponent } from './add-tutorial/add-tutorial.component';
 import { TutorialDetailsComponent } from './tutorial-details/tutorial-details.component';
 import { TutorialListComponent } from './tutorial-list/tutorial-list.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AddTutorialComponent,
     TutorialDetailsComponent,
-    TutorialListComponent
+    TutorialListComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
