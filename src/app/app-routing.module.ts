b46fad1a0e048c852770d41fa08b1b47e2dc8132
Abzed <ibraabzed@gmail.com>
2020-11-05 16:21:38 +0300

    add routing to app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TutorialListComponent } from './tutorial-list/tutorial-list.component';
+import { TutorialDetailsComponent } from './tutorial-details/tutorial-details.component';
+import { AddTutorialComponent } from './add-tutorial/add-tutorial.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'tutorials', pathMatch: 'full' },
+  { path: 'tutorials', component: TutorialListComponent },
+  { path: 'tutorials/:id', component: TutorialDetailsComponent },
+  { path: 'add', component: AddTutorialComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
