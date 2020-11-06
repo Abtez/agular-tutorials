9f94904328bbd2eb2530d7638b9463cf514cd1f7
Abzed <ibraabzed@gmail.com>
2020-11-06 06:33:14 +0300

    add looping of tutorials in main page

--- src/app/tutorial-details/tutorial-details.component.ts
+++ src/app/tutorial-details/tutorial-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TurorialserviceService } from 'src/app/turorialservice.service';
+import { Tutorials} from '../tutorials'
 
 @Component({
   selector: 'app-tutorial-details',
--- src/app/tutorial-list/tutorial-list.component.ts
+++ src/app/tutorial-list/tutorial-list.component.ts
@@ -9,9 +9,62 @@ import { Tutorials} from '../tutorials'
 })
 export class TutorialListComponent implements OnInit {
 
-  constructor() { }
+  tutorials: any;
+  currentTutorial = null;
+  currentIndex = -1;
+  title = '';
+
+  constructor(private tutorialService: TurorialserviceService) { }
 
   ngOnInit(): void {
+    this.retrieveTutorials();
+  }
+
+  retrieveTutorials(): void {
+    this.tutorialService.getAll()
+      .subscribe(
+        data => {
+          this.tutorials = data;
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  refreshList(): void {
+    this.retrieveTutorials();
+    this.currentTutorial = null;
+    this.currentIndex = -1;
+  }
+
+  setActiveTutorial(tutorial, index): void {
+    this.currentTutorial = tutorial;
+    this.currentIndex = index;
+  }
+
+  removeAllTutorials(): void {
+    this.tutorialService.deleteAll()
+      .subscribe(
+        response => {
+          console.log(response);
+          this.retrieveTutorials();
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  searchTitle(): void {
+    this.tutorialService.findByTitle(this.title)
+      .subscribe(
+        data => {
+          this.tutorials = data;
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        });
   }
 
 }
